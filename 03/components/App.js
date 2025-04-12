785ff1d2f85f584a0a81bf7511b049820841423b
Marek Bełz <belzmarek@gmail.com>
2025-04-12 12:36:05 +0000

    task 03 done

--- 03/components/App.js
+++ 03/components/App.js
@@ -1,15 +1,19 @@
-// ./src/components/App.js
 import React from 'react';
 import Box from './Box';
 import Div from './Div';
+import { TextContext, ColorContext } from '../context';
 
-const App = () => {
+function App() {
     return (
-        <section>
-            <Box />
-            <Div />
-        </section>
+        <ColorContext.Provider value="red">
+            <section>
+                <Box />
+                <TextContext.Provider value="sibling">
+                    <Div />
+                </TextContext.Provider>
+            </section>
+        </ColorContext.Provider>
     );
-};
+}
 
 export default App;
--- 03/components/Box.js
+++ 03/components/Box.js
@@ -1,9 +1,8 @@
-// ./src/components/Box.js
 import React from 'react';
 import Div from './Div';
 
-const Box = () => {
+function Box() {
     return <Div />;
-};
+}
 
 export default Box;
