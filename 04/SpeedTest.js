57aa801a5e09161060a1b7f7297382d9a5249046
Marek Bełz <belzmarek@gmail.com>
2025-04-13 12:25:25 +0000

    task 04 done

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,19 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useRandomItem from './hook';
 
-const SpeedTest = () => {
-    const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+function SpeedTest() {
+    const [word, regenerateWord] = useRandomItem([
+        'devmentor.pl',
+        'abc',
+        'JavaScript',
+        'Cokolwiek',
+        'Konstantynopolitańczykowianeczka',
+    ]);
+    const [inputValue, setInputValue] = useState('');
+    const [time, setTime] = useState(0);
+    const [totalChars, setTotalChars] = useState(0);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const handleFocus = () => {
+        if (!intervalRef.current) {
+            intervalRef.current = setInterval(() => {
+                setTime((prevTime) => prevTime + 1);
+            }, 1000);
+        }
+    };
+
+    const handleBlur = () => {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+    };
+
+    const handleChange = ({ target: { value } }) => {
+        setInputValue(value);
+
+        if (value === word) {
+            setTotalChars((prevTotal) => prevTotal + word.length);
+            setInputValue('');
+            regenerateWord();
+        }
+    };
 
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <p>Time: {time} seconds</p>
+            <p>Total characters typed: {totalChars}</p>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
         </div>
     );
-};
+}
 
 export default SpeedTest;
